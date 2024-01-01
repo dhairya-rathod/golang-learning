aa7f7c95a5378cde29a1c6ac1297a0fc17da27c5
Dhairya <dhairya.rathod24@gmail.com>
2024-01-01 22:09:18 +0530

    GoLang :: first class functions

--- 15-functions/main.go
+++ 15-functions/main.go
@@ -11,6 +11,7 @@ func main() {
 				logic...
 				return
 			}
+		-> Functions in golang are First-class citizens (same as javascript) which means we can use it as a variable, can be return from another function and can be passed as an argument to another function
 	*/
 
 	greetUser("John")
@@ -19,7 +20,23 @@ func main() {
 	flName, length := fullNameWithLength("Emily", "Backwood")
 	fmt.Println(flName, length)
 
-	variadicParam()
+	// variadicParam()
+
+	// first-class function
+	fnEx := func() {
+		println("Yo boiii")
+	}
+	fnEx()
+
+	// function as argument
+	sayNamaste("Emily", "Backwood", fullName)
+
+	// currying function
+	abc := curryingFn(7)(7)
+	sumFive := curryingFn(5)
+	ans := sumFive(4)
+	ans2 := sumFive(10)
+	println(abc, ans, ans2)
 }
 
 // returns nothing
@@ -37,4 +54,20 @@ func fullNameWithLength(fName, lName string) (string, int) {
 	flName := fullName(fName, lName)
 
 	return flName, len(flName)
+}
+
+// function as a param
+func sayNamaste(fName, lName string, fn func(string, string) string) {
+	fullName := fn(fName, lName)
+
+	fmt.Println("Namaste ", fullName)
+}
+
+// return function from a function
+type fnType func(int) int // function type for code readability
+
+func curryingFn(a int) fnType {
+	return func(b int) int {
+		return a + b
+	}
 }
